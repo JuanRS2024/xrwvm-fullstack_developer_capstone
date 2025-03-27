fcd35a6f04dd2cfc2effd6ddc2e72118b0aa1cc6
JUANRS <juan_sk8_@hotmail.com>
2025-03-27 03:24:46 +0100

    Update settings.py
    fix issues.

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,10 +31,11 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     'https://juanromero1-8000.theiadockernext-0-labs-prod-'
-        'theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://juanromero1-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://juanromero1-8000.theiadockernext-0-labs-prod-'
+    'theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
@@ -105,20 +106,28 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'MinimumLengthValidator'
+        ),
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'CommonPasswordValidator'
+        ),
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'NumericPasswordValidator'
+        ),
     },
 ]
 
